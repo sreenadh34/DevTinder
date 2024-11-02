6cbfdc6a7c67f603c04734d6c181ecc282d2ae76
sreenadh34 <43.sreenadh@gmail.com>
2024-11-03 00:33:41 +0530

    Add User model

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import connectDB  from "./config/database.js";
+import User from "./models/user.js";
 
 
 connectDB();
@@ -7,13 +8,16 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-// Add new route for status
-app.get("/status", (req, res) => {
-  res.json({ status: "Server is running smoothly" });
+app.post("/signup", async (req,res) => {
+  // creting a new instance of User model
+  const user = new User({
+    firstName: "sreenadh1",
+    lastName: "M",
+    emailId:"sreendh1.34@mail.com",
+    password:"hufhrufhfurhfurh"
+  });
+  await user.save();
+  res.send("User added successfully");
 });
 
 app.listen(PORT, () => {
