92c29bdc0d12767d563721fd291646293f52d810
sreenadh34 <43.sreenadh@gmail.com>
2024-11-04 01:43:25 +0530

    Delete API

--- src/app.js
+++ src/app.js
@@ -20,6 +20,43 @@ app.post("/signup", async (req,res) => {
     res.status(400).send("Error saving user" + err.message );
   }
 });
+// Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.find({ emailId: userEmail });
+      if (users.length === 0) {
+      res.status(404).send("User not found");
+     } else {
+      res.send(users);
+     }
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
+// Detele a user from the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+  const user = await User.findByIdAndDelete(userId);
+
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
