01111c94c124b6cae3bedb45da1cc7bbfb62d383
sreenadh34 <43.sreenadh@gmail.com>
2024-11-04 00:49:29 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -8,16 +8,17 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json())
+
 app.post("/signup", async (req,res) => {
   // creting a new instance of User model
-  const user = new User({
-    firstName: "sreenadh1",
-    lastName: "M",
-    emailId:"sreendh1.34@mail.com",
-    password:"hufhrufhfurhfurh"
-  });
+  const user = new User(req.body);
+  try{
   await user.save();
   res.send("User added successfully");
+  } catch(err) {
+    res.status(400).send("Error saving user" + err.message );
+  }
 });
 
 app.listen(PORT, () => {
