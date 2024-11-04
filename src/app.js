aa95e57cb1c0cf0c47e5126b6dc4a1781002f2e8
sreenadh34 <43.sreenadh@gmail.com>
2024-11-05 01:14:08 +0530

    Enhance user model validation and error handling in API

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ app.patch("/user", async (req, res) => {
     });
     res.send("User updated successfully");
   } catch (err) {
-    res.status(400).send("Something went wrong ");
+    res.status(400).send("UPDATE FAILED:" + err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,55 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
         firstName:{
-            type: String
+            type: String,
+            required: true,
+            minLength: 4,
+            maxLength: 50,
         },
         lastName: {
             type: String,
+            minLength: 4,
+            maxLength: 10,
         },
         emailId: {
            type: String,
+           lowercase: true,
+           required: true,
+           unique: true,
+          trim: true,
         },
         password:{
             type: String,
+            required: true,
         },
        age: {
          type: Number,
+         min: 18,
         } ,
        gender: {
          type: String,
-       }
-});
+         validate(value) {
+            if (!["male", "female", "others"].includes(value)) {
+              throw new Error("Gender data is not valid");
+            }
+         },
+       },
+        photoUrl: {
+         type: String,
+         default: "https://geographyandyou.com/images/user-profile.png",
+       },
+        about: {
+        type: String,
+        default: "This is a default about of the user!",
+      },
+       skills: {
+       type: [String],
+      },
+ },
+   {
+    timestamps: true,
+  }
+ );
 
 const User = mongoose.model('User', userSchema);
 
